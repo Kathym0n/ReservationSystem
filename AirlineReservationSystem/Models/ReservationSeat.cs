9bdb30c4d3b2143611b7fa2c54f1f6f39b33ca8c
Katharina Riedl <katharina.riedl@inosim.com>
2026-02-25 15:26:23 +0100

    Add WPF UI and NUnit test projects to solution
    Added AirlineReservationSystem.UI as a WPF application with initial boilerplate files. Introduced AirlineReservationSystem.UnitTests with NUnit and a sample test to verify setup. Reformatted ReservationSeat.cs and corrected its namespace. Updated ReservationSystem.sln to include and configure the new projects.

--- AirlineReservationSystem/Models/ReservationSeat.cs
+++ AirlineReservationSystem/Models/ReservationSeat.cs
@@ -1,9 +1,8 @@
-﻿using AirlineReservationSystem.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace AirlineReservationSystem
+namespace AirlineReservationSystem.Models
 {
     public class ReservationSeat : IHasID
     {
